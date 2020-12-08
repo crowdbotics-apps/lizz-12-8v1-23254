652094c0a1097298bb666d78e91f583c70b878ac
Crowdbotics <team@crowdbotics.com>
2020-12-08 17:24:24 +0000

    Update components

--- src/features/BlankScreen1183280/screens/blank.js
+++ src/features/BlankScreen1183280/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +10,49 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <Text style={styles.Text_3}>This is screen 1</Text>
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("BlankScreen0183279")}
+      >
+        <Text>Select this text to go to screen 0</Text>
+      </TouchableOpacity>
+      <Text>Select below button to show an alert</Text>
+      <Button
+        title="Shows an alert"
+        color="#b39eff"
+        onPress={() => alert("Pressed!")}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
+  View_1: {},
+  Text_3: {
+    fontSize: 32,
+    color: "#22dd41",
+    fontFamily: "PlayfairDisplay-BoldItalic",
+    lineHeight: 32
   },
-});
+  Text_4: {},
+  Text_5: {},
+  Button_7: {}
+})
