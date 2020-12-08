6036837d21c9e0536fe1396125bcc0d5f07bede8
Crowdbotics <team@crowdbotics.com>
2020-12-08 20:16:51 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,37 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(
+        max_length=150,
+    )
+    test = models.DecimalField(
+        max_digits=30,
+        decimal_places=10,
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +45,26 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+
+class Test(models.Model):
+    "Generated Model"
+    test = models.BigIntegerField()
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="test_user",
+    )
--- backend/lizz_12_8v1_23254/wsgi.py
+++ backend/lizz_12_8v1_23254/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lizz_12_8v1_23254.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lizz_12_8v1_23254.settings")
 
 application = get_wsgi_application()
