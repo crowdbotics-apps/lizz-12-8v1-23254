82d1fcf307713021ba841946581e44e59f928f78
Crowdbotics <team@crowdbotics.com>
2021-01-05 19:18:36 +0000

    Update components

--- src/features/BlankScreen11190463/screens/blank.js
+++ src/features/BlankScreen11190463/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +10,62 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <Text>Sample text content</Text>
+      <Image
+        source={{
+          uri:
+            "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/plant_lnFvaGo.jpg"
+        }}
+      />
+      <View style={styles.View_6}>
+        <View>
+          <Button
+            title="Like"
+            color="#aeec3c"
+            style={styles.Button_47}
+            onPress={() => alert("Pressed!")}
+          />
+        </View>
+        <View>
+          <Button
+            title="Like"
+            color="#aeec3c"
+            style={styles.Button_48}
+            onPress={() => alert("Pressed!")}
+          />
+        </View>
+      </View>
+      <Text>Sample text content</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
-  },
-});
+  View_1: {},
+  Text_4: {},
+  Image_5: {},
+  View_6: { width: "100%", flexDirection: "row" },
+  View_7: {},
+  Button_47: { fontSize: 22, fontFamily: "Montserrat-Regular" },
+  View_8: {},
+  Button_48: { fontSize: 22, fontFamily: "Montserrat-Regular" },
+  Text_9: {}
+})
