76e0c71435607c0becfed968e5d471a099e4a5aa
Crowdbotics <team@crowdbotics.com>
2021-01-05 19:18:52 +0000

    Update store

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,11 +3,13 @@ export const signup_post_api_v1_signup_read = data => ({
   type: types.SIGNUP_POST_API_V1_SIGNUP_READ,
   data
 })
+
 export const signup_post_api_v1_signup_readSucceeded = (response, starter) => ({
   type: types.SIGNUP_POST_API_V1_SIGNUP_READ_SUCCEEDED,
   response,
   starter
 })
+
 export const signup_post_api_v1_signup_readFailed = (response, starter) => ({
   type: types.SIGNUP_POST_API_V1_SIGNUP_READ_FAILED,
   response,
